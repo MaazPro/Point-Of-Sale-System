bb79d0502d9db98e283b26c3dcf42233244768e1
MaazPro <maazmuhammad94@gmail.com>
2022-05-04 17:30:26 +0800

    decriment counter completed

--- process.php
+++ process.php
@@ -5,7 +5,7 @@ $quantity = 0;
 $itemCost = 0;
 
 if(isset($_POST['p1'])){
-    echo "p1: ";
+    // echo "p1: ";
     //unique order id
     $order_id = rand(10,100) + rand(1,10);
     $cost_per_item = 2;
@@ -17,8 +17,47 @@ if(isset($_POST['p1'])){
     die($mysqli->error);
     header("location: index.php");
 }
+if(isset($_POST['p2'])){
+    // echo "p1: ";
+    //unique order id
+    $order_id = rand(10,100) + rand(1,10);
+    $cost_per_item = 4;
+    $product_name = "P2";
+    $quantity = 1;
+    
+    // write to db order_item
+    $mysqli->query("INSERT INTO order_item (order_id, cost_per_item, product_name, quantity) VALUES ('$order_id','$cost_per_item','$product_name','$quantity')") or
+    die($mysqli->error);
+    header("location: index.php");
+}
+if(isset($_POST['p3'])){
+    // echo "p1: ";
+    //unique order id
+    $order_id = rand(10,100) + rand(1,10);
+    $cost_per_item = 8;
+    $product_name = "P3";
+    $quantity = 1;
+    
+    // write to db order_item
+    $mysqli->query("INSERT INTO order_item (order_id, cost_per_item, product_name, quantity) VALUES ('$order_id','$cost_per_item','$product_name','$quantity')") or
+    die($mysqli->error);
+    header("location: index.php");
+}
+if(isset($_POST['p4'])){
+    // echo "p1: ";
+    //unique order id
+    $order_id = rand(10,100) + rand(1,10);
+    $cost_per_item = 16;
+    $product_name = "P4";
+    $quantity = 1;
+    
+    // write to db order_item
+    $mysqli->query("INSERT INTO order_item (order_id, cost_per_item, product_name, quantity) VALUES ('$order_id','$cost_per_item','$product_name','$quantity')") or
+    die($mysqli->error);
+    header("location: index.php");
+}
 
-
+// Increment quantity
 if(isset($_GET['add'])){
     $id = $_GET['add'];
     
@@ -28,11 +67,31 @@ if(isset($_GET['add'])){
     $counter = $row['quantity'];
     $counter++;
     $mysqli->query("UPDATE order_item SET quantity= '$counter' WHERE id = '$id'") or die($mysqli->error());
-    echo $row['quantity'];
+    // echo $row['quantity'];
     // echo $counter;
-    
       header("location: index.php");
   }
 
+//   Decrement quantity
+if(isset($_GET['subtract'])){
+    $id = $_GET['subtract'];
+    
+    // "SELECT quantity FROM order_item WHERE id = '$id' "
+    $result = $mysqli->query("SELECT quantity FROM order_item WHERE id = '$id' ") or die ($mysqli->error);
+    $row = $result-> fetch_assoc();
+    $counter = $row['quantity'];
+    $counter--;
+    if($counter < 1){
+        // DELETE FROM `order_item` WHERE `id` = 27
+    $result = $mysqli->query("DELETE FROM order_item WHERE id = '$id'") or die ($mysqli->error);
+
+    }else{
+
+        $mysqli->query("UPDATE order_item SET quantity= '$counter' WHERE id = '$id'") or die($mysqli->error());
+    }
+    // echo $row['quantity'];
+    // echo $counter;
+      header("location: index.php");
+  }
 
 ?>
