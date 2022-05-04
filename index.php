ee3c5cc591fea5547a986be3e30bf515f6594f94
MaazPro <maazmuhammad94@gmail.com>
2022-05-04 22:52:14 +0800

    Model Design in progress

--- index.php
+++ index.php
@@ -139,8 +139,50 @@
         <br><br>
         <center>
         <button type="submit" class = "btn btn-danger" name = "cancel">Cancel</button>
-        <!-- &nbsp; &nbsp;  -->
-        <button type="submit" class = "btn btn-success" name = "checkout">Checkout</button>
+        <!-- <button type="submit" class = "btn btn-success" name = "checkout" data-toggle="modal" data-target="#exampleModal">Checkout</button> -->
+        <button type="button" class = "btn btn-success" name = "checkout" data-toggle="modal" data-target="#exampleModal">Checkout</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <table class = "table">
+                    <thead>
+                    <tr>
+                        <th colspan = "3">Total Paid Amount</th>
+                        <th></th>
+                    </tr>  
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>  
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>  
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>  
+                  </thead>
+                
+                  </table>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary" name = "submit">Submit</button>
+                </div>
+              </div>
+            </div>
+        </div>
+        
         </center>
         </form>  
       </div>
